b01ce263a681832474628de45abbc5a87ae47835
Shishova <shishova.alena2013@yandex.ru>
2020-10-16 01:10:10 +0300

    1.3	Создать класс Point (точка) с тремя публичными неизменяемыми (final) координатами-полями типа double: x, y и z. Создать единственный конструктор, который требует задания всех трёх координат. Создать несколько точек.

--- task0/src/main/java/ru/ssau/tk/oop/task0/MainClass.java
+++ task0/src/main/java/ru/ssau/tk/oop/task0/MainClass.java
@@ -2,35 +2,8 @@ package ru.ssau.tk.oop.task0;
 
 public class MainClass {
     public static void main(String[] args) {
-        Person sister = new Person();
-        sister.setLastName("Shishova");
-        sister.setFirstName("Elena");
-        sister.setPassportId(3216450);
-
-        Person brother = new Person();
-        brother.setLastName("Shishov");
-        brother.setFirstName("Andrey");
-        brother.setPassportId(1078645);
-
-        System.out.println(sister.getFirstName() + ' ' + sister.getLastName() + ' ' + sister.getPassportId());
-        System.out.println(brother.getFirstName() + ' ' + brother.getLastName() + ' ' + brother.getPassportId());
-
-        Person friend = new Person();
-        friend.setFirstName("Marina");
-        friend.setLastName("Kolebanova");
-        friend.setPassportId(777456777);
-
-        System.out.println(friend.getFirstName() + ' ' + friend.getLastName() + ' ' + friend.getPassportId());
-
-        Person myBestFriend = new Person("Karina", "Myshibanova");
-        System.out.println(myBestFriend.getFirstName() + ' ' + myBestFriend.getLastName());
-
-        Person actor = new Person(789654);
-        actor.setLastName("Depp");
-        actor.setFirstName("Johnny");
-        System.out.println(actor.getFirstName() + ' ' + actor.getLastName() + ' ' + actor.getPassportId());
-
-        Person singer = new Person("Sergei", "ATL", 321456111);
-        System.out.println(singer.getFirstName() + ' ' + singer.getLastName() + ' ' + singer.getPassportId());
+        Point firstPoint = new Point(1, 1, 1);
+        Point secondPoint = new Point(2, 2, 2);
+        Point thirdPoint = new Point(3, 3, 3);
     }
 }
---
+++ task0/src/main/java/ru/ssau/tk/oop/task0/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.oop.task0;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
