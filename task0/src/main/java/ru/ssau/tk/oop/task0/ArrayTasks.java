a1a713b8c85b7bc52df2d7bde663d1c06cb4cb65
Shishova <shishova.alena2013@yandex.ru>
2020-11-09 12:43:46 +0300

    2.4	Создать метод, генерирующий массив чисел по входной размерности. Заполнить массив последовательностью убывающих чётных чисел, так, чтобы последним элементом была 2 (например, 10, 8, 6, 4, 2). Вернуть массив в качестве результата и покрыть метод тестами.

--- task0/src/main/java/ru/ssau/tk/oop/task0/ArrayTasks.java
+++ task0/src/main/java/ru/ssau/tk/oop/task0/ArrayTasks.java
@@ -1,12 +1,12 @@
 package ru.ssau.tk.oop.task0;
 
 public class ArrayTasks {
-    public int[] generateArray(int l) {
+    public int[] generateArrayDecreasingEven(int l) {
         int[] myArray = new int[l];
-        int k = 1;
+        int k = 2 * l;
         for (int i = 0; i < l; i++) {
             myArray[i] = k;
-            k = k + 2;
+            k = k - 2;
         }
         return myArray;
     }
--- task0/src/test/java/ru/ssau/tk/oop/task0/ArrayTasksTest.java
+++ task0/src/test/java/ru/ssau/tk/oop/task0/ArrayTasksTest.java
@@ -9,11 +9,11 @@ public class ArrayTasksTest {
     @Test
     public void testGenerateArray() {
         ArrayTasks check = new ArrayTasks();
-        int[] newArray = check.generateArray(5);
-        assertEquals(newArray[0], 1);
-        assertEquals(newArray[1], 3);
-        assertEquals(newArray[2], 5);
-        assertEquals(newArray[3], 7);
-        assertEquals(newArray[4], 9);
+        int[] newArray = check.generateArrayDecreasingEven(5);
+        assertEquals(newArray[0], 10);
+        assertEquals(newArray[1], 8);
+        assertEquals(newArray[2], 6);
+        assertEquals(newArray[3], 4);
+        assertEquals(newArray[4], 2);
     }
 }
