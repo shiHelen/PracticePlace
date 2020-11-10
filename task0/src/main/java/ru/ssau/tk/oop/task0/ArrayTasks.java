5faf65e0966ad34f518f2482c5cc9ca2f6ed8892
Shishova <shishova.alena2013@yandex.ru>
2020-11-10 21:22:19 +0300

    вернула предыдущие номера из задач по массивам + 2.7	Создать метод, принимающий на вход a, b и c – дробные числа, соответствующие коэффициентам квадратного уравнения. Метод должен возвращать решение в виде массива: если решений нет, то массив должен быть пуст (0 элементов). Если решение одно, то массив должен иметь размерность 1 и в нём должно содержаться это единственное решение. Если решений два, то массив должен иметь размерность 2 и содержать в себе оба решения в порядке возрастания. Покрыть метод тестами (с заданной точностью). Не забывать, что a может быть равно 0.

--- task0/src/main/java/ru/ssau/tk/oop/task0/ArrayTasks.java
+++ task0/src/main/java/ru/ssau/tk/oop/task0/ArrayTasks.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.oop.task0;
 
+import static java.lang.Math.sqrt;
+
 public class ArrayTasks {
     static double[] getArithmeticProgression(int n, double a1, double d) {
         double[] array = new double[n];
@@ -8,5 +10,84 @@ public class ArrayTasks {
             array[i] = array[i - 1] + d;
         }
         return array;
+    }
+
+    public int[] arrayLength(int l) {
+        return new int[l];
+    }
+
+    public int[] generateOnesArray(int l) {
+        int[] ones = new int[l];
+        for (int i = 0; i < l; i++) {
+            ones[i] = 1;
+        }
+        ones[0] = 2;
+        ones[l - 1] = 2;
+        return ones;
+    }
+
+    public int[] generateArray(int l) {
+        int[] myArray = new int[l];
+        int k = 1;
+        for (int i = 0; i < l; i++) {
+            myArray[i] = k;
+            k = k + 2;
+        }
+        return myArray;
+    }
+
+    public int[] generateArrayDecreasingEven(int l) {
+        int[] myArray = new int[l];
+        int k = 2 * l;
+        for (int i = 0; i < l; i++) {
+            myArray[i] = k;
+            k = k - 2;
+        }
+        return myArray;
+    }
+
+    public int[] generateArrayFibonacciNumbers(int l) {
+        if (l == 1) {
+            return new int[]{1};
+        }
+        if (l == 2) {
+            return new int[]{1, 1};
+        }
+        int[] fibArray = new int[l];
+        fibArray[0] = 1;
+        fibArray[1] = 1;
+        for (int i = 2; i < l; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+        }
+        return fibArray;
+    }
+
+    public int[] generateArraySqrIndex(int l) {
+        int[] taskArray = new int[l];
+        for (int i = 0; i < l; i++) {
+            taskArray[i] = i * i;
+        }
+        return taskArray;
+    }
+
+    public double[] quadraticSolution(double a, double b, double c) {
+        double D = b * b - 4 * a * c;
+        if ((D == 0) & (a > 0)) {
+            double[] result = new double[1];
+            result[0] = -b / (2 * a);
+            return result;
+        }
+        if ((D > 0) & (a > 0)) {
+            double[] result = new double[2];
+            result[0] = (-b - sqrt(D)) / (2 * a);
+            result[1] = (-b + sqrt(D)) / (2 * a);
+            return result;
+        }
+        if (a == 0) {
+            double[] result = new double[1];
+            result[0] = -c / b;
+            return result;
+        }
+        return new double[0];
     }
 }
--- task0/src/test/java/ru/ssau/tk/oop/task0/ArrayTasksTest.java
+++ task0/src/test/java/ru/ssau/tk/oop/task0/ArrayTasksTest.java
@@ -7,9 +7,84 @@ import static org.testng.Assert.*;
 public class ArrayTasksTest {
 
     @Test
+    public void testArrayLength() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.arrayLength(3);
+        assertEquals(newArray.length, 3);
+        int[] othArray = check.arrayLength(5);
+        assertEquals(othArray.length, 5);
+        assertNotEquals(othArray.length, 4);
+    }
+
+    @Test
     public void testGetArithmeticProgression() {
         assertEquals(ArrayTasks.getArithmeticProgression(2, 5, -2), new double[]{5, 3});
         assertEquals(ArrayTasks.getArithmeticProgression(5, -1, 2), new double[]{-1, 1, 3, 5, 7});
         assertEquals(ArrayTasks.getArithmeticProgression(3, 0, 4), new double[]{0, 4, 8});
+    }
+
+    @Test
+    public void testGenerateOnesArray() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.generateOnesArray(5);
+        assertEquals(newArray[0], 2);
+        assertEquals(newArray[1], 1);
+        assertEquals(newArray[2], 1);
+        assertEquals(newArray[3], 1);
+        assertEquals(newArray[4], 2);
+    }
+
+    @Test
+    public void testGenerateArray() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.generateArray(5);
+        assertEquals(newArray[0], 1);
+        assertEquals(newArray[1], 3);
+        assertEquals(newArray[2], 5);
+        assertEquals(newArray[3], 7);
+        assertEquals(newArray[4], 9);
+    }
+
+    @Test
+    public void testGenerateArrayDecreasingEven() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.generateArrayDecreasingEven(5);
+        assertEquals(newArray[0], 10);
+        assertEquals(newArray[1], 8);
+        assertEquals(newArray[2], 6);
+        assertEquals(newArray[3], 4);
+        assertEquals(newArray[4], 2);
+    }
+
+    @Test
+    public void testGenerateArrayFibonacciNumbers() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.generateArrayFibonacciNumbers(6);
+        assertEquals(newArray[0], 1);
+        assertEquals(newArray[1], 1);
+        assertEquals(newArray[2], 2);
+        assertEquals(newArray[3], 3);
+        assertEquals(newArray[4], 5);
+        assertEquals(newArray[5], 8);
+    }
+
+    @Test
+    public void testGenerateArraySqrIndex() {
+        ArrayTasks check = new ArrayTasks();
+        int[] newArray = check.generateArraySqrIndex(6);
+        assertEquals(newArray[0], 0);
+        assertEquals(newArray[1], 1);
+        assertEquals(newArray[2], 4);
+        assertEquals(newArray[3], 9);
+        assertEquals(newArray[4], 16);
+        assertEquals(newArray[5], 25);
+    }
+
+    @Test
+    public void testQuadraticSolution() {
+        ArrayTasks checkSolution = new ArrayTasks();
+        assertEquals(checkSolution.quadraticSolution(2, 4, -16), new double[]{-4, 2}, 0.001);
+        assertEquals(checkSolution.quadraticSolution(0, 4, -2), new double[]{0.5}, 0.001);
+        assertEquals(checkSolution.quadraticSolution(1, 4, 4), new double[]{-2}, 0.001);
     }
 }
