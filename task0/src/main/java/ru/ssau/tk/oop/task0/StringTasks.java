09650e47125ce9554a9d78c423ac32504491b8b2
Shishova <shishova.alena2013@yandex.ru>
2020-11-14 13:24:18 +0300

    2.2	Создать метод, принимающий на вход строку String. У строки необходимо получить массив байт с помощью метода getBytes(), затем требуется пробежаться по всему массиву с помощью цикла for-each и вывести каждый байт с новой строки. Проверить, как отличаются выводы, если строка содержит латинские и/или русские символы. у русских символов отрицательные значения

--- task0/src/main/java/ru/ssau/tk/oop/task0/StringTasks.java
+++ task0/src/main/java/ru/ssau/tk/oop/task0/StringTasks.java
@@ -1,18 +1,31 @@
 package ru.ssau.tk.oop.task0;
 
 public class StringTasks {
-    public void outputSeparately(String sentence) {
+    public void outputSeparately(String sentence) {     //3.1 вывод строки в консоль отдельно по одному символу в строке
         for (int i = 0; i < sentence.length(); i++) {
             System.out.println(sentence.charAt(i));
         }
     }
 
+    public void getArrayOfBytes(String str) {        //3.2 получить массив байтов из строки и вывести его
+        byte[] bytesValue = str.getBytes();
+        for (int bytes : bytesValue) {
+            System.out.println(bytes);
+        }
+    }
+
+    public static StringTasks check = new StringTasks();
+
     public static void main(String[] args) {
-        StringTasks check = new StringTasks();
+        /*  вызов метода 3.1 все работает, но уже не актуально, поэтому в комментарии
         check.outputSeparately("abstract");
         System.out.println("");
         check.outputSeparately("not bad");
         System.out.println("");
-        check.outputSeparately("it's work yeees!");
+        check.outputSeparately("it works yeees!");
+        */
+        check.getArrayOfBytes("проверка");
+        System.out.println("");
+        check.getArrayOfBytes("something");
     }
 }
